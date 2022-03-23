bf56d8bbcccec190dbd5adca48a60060af96b9f8
Phil123095 <68194774+Phil123095@users.noreply.github.com>
2022-03-23 16:29:56 +0100

    modified filterbar

--- src/containers/Dashboard/Dashboard Elements/FilterBar.js
+++ src/containers/Dashboard/Dashboard Elements/FilterBar.js
@@ -5,8 +5,6 @@ import Select from 'react-select'
 function FilterBar(props) {
     const [selectedDate, setSelectedDate] = useState(['2021-01-01', '2021-01-04']);
     const [selectedGroup, setSelectedGroup] = useState(['IBERIA']);
-    const [minDate, setMinDate] = useState();
-    const [maxDate, setMaxDate] = useState();
 
     const handleDateChange = (e) => {
         setSelectedDate(Array.isArray(e) ? e.map(x => x.value) : []);
@@ -32,13 +30,8 @@ function FilterBar(props) {
 
     const handleSubmit = (event) => {
         console.log(selectedDate)
-        if ((!minDate && !maxDate) || selectedDate.length === 0) {
-            setMinDate('2021-01-01')
-            setMaxDate('2021-04-01')
-        } else {
-            setMinDate(selectedDate.reduce((acc,date)=>{return acc&&new Date(acc)<new Date(date)?acc:date},''))
-            setMaxDate(selectedDate.reduce((acc,date)=>{return acc&&new Date(acc)>new Date(date)?acc:date},''))
-        }
+        const minDate = selectedDate.length === 0 ? '2021-01-01' : selectedDate.reduce((acc,date)=>{return acc&&new Date(acc)<new Date(date)?acc:date},'');
+        const maxDate = selectedDate.length === 0 ? '2021-04-01' : selectedDate.reduce((acc,date)=>{return acc&&new Date(acc)>new Date(date)?acc:date},'');
         props.getDataFromAPI({minDate, maxDate, selectedGroup})
     }
 
